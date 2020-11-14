1e0948b38e630633804ac2bd29d13e068563fb8d
DerekBiii <73568223+DerekBiii@users.noreply.github.com>
2020-11-14 11:22:05 -0500

    second burger

--- index.js
+++ index.js
@@ -26,3 +26,32 @@ const navSlide = () => {
 }
 
 navSlide();
+
+const navSlide2 = () => {
+    const burger = document.querySelector('.burger2');
+    const nav = document.querySelector('.nav-links2');
+    const navLinks = document.querySelectorAll('.nav-links2 li');
+    
+    burger.addEventListener('click',()=>{
+
+        //Toggle Nav
+        nav.classList.toggle('nav-active2'); 
+
+        //Animate Links
+        navLinks.forEach((link,index)=>{
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation =`navLinkFade2 0.5s ease backwards ${index / 7 + 0.3 }s`
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+    
+    
+
+}
+
+navSlide2();
